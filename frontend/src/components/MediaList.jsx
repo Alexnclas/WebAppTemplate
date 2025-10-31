0ed8eec9d0d3f91d5c64b1ae22e98bd8dbd9d382
Buska909 <alexnclas@gmail.com>
2025-10-31 19:45:41 -0300

    rework nginx try

--- frontend/src/components/MediaList.jsx
+++ frontend/src/components/MediaList.jsx
@@ -3,7 +3,7 @@ import MediaItem from "./MediaItem";
 export default function MediaList({ medias }) {
     return(
     <>
-        {medias.map(media => (
+        {Array.isArray(medias) && medias.map(media => (
             <MediaItem key={media._id} media={media}></MediaItem>
         ))}
     </>
